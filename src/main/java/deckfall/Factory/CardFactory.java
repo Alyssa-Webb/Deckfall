3d6abdaf437d0a3111dc27c6fc36209f73721e2b
Alyssa Webb <amarie.19050874@gmail.com>
2026-03-20 03:23:24 -0600

    Add factories

---
+++ src/main/java/deckfall/Factory/CardFactory.java
@@ -0,0 +1,12 @@
+package deckfall.Factory;
+
+import deckfall.Card.*;
+
+public class CardFactory {
+    public static Card createCard(CardType type) {
+        return switch (type) {
+            case SLASH -> new SlashCard();
+            case SHIELD -> new ShieldCard();
+        };
+    }
+}
