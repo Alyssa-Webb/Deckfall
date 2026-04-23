1b7a0f44e94576b912084c9eeef03efd3c4fd0a1
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-23 14:43:13 -0600

    Create tests for Enemy class

--- src/test/java/deckfall/Entity/EnemyTest.java
+++ src/test/java/deckfall/Entity/EnemyTest.java
@@ -1,5 +1,38 @@
 package deckfall.Entity;
 
-public class EnemyTest {
+import deckfall.Factory.EnemyFactory;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTests {
+
+    @Test
+    void createEnemyReturnsSkeleton() {
+        assertInstanceOf(Skeleton.class, EnemyFactory.createEnemy(EnemyType.SKELETON));
+    }
+
+    @Test
+    void createEnemyReturnsGoblin() {
+        assertInstanceOf(Goblin.class, EnemyFactory.createEnemy(EnemyType.GOBLIN));
+    }
+
+    @Test
+    void createEnemyReturnsTroll() {
+        assertInstanceOf(Troll.class, EnemyFactory.createEnemy(EnemyType.TROLL));
+    }
+
+    @Test
+    void createRandomEnemy() {
+        Enemy enemy = EnemyFactory.createRandomEnemy();
+        assertNotNull(enemy);
+        assertTrue(enemy.isAlive());
+    }
+
+    @Test
+    void createCustomEnemy() {
+        Enemy enemy = EnemyFactory.createEnemy(EnemyType.SKELETON, "Skelly", 1);
+        assertEquals("Skelly", enemy.getName());
+        assertEquals(1, enemy.getHP());
+    }
 }
