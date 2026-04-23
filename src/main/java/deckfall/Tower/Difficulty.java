713456bf20a0d5ea0bc7759e3db01b9447cf76e2
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-23 14:24:29 -0600

    Added Difficulty enum to condense multiple difficulties from several classes

---
+++ src/main/java/deckfall/Tower/Difficulty.java
@@ -0,0 +1,13 @@
+package deckfall.Tower;
+
+public enum Difficulty {
+    // Level Difficulty
+    EARLYGAME,
+    LATEGAME,
+    ENDGAME,
+
+    // Tower/Scaling Difficulty
+    EASY,
+    MEDIUM,
+    HARD
+}
