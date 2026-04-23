1d47108e7800b7463a1b92ac2071eaba7ba766fe
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-23 17:12:33 -0600

    Update main to run GUI (temp)

--- src/main/java/deckfall/Main.java
+++ src/main/java/deckfall/Main.java
@@ -4,15 +4,26 @@ import deckfall.Controller.GameController;
 import deckfall.Entity.Slayer;
 import deckfall.Game.Game;
 import deckfall.Observer.ConsoleLogger;
+import deckfall.Observer.GameEventObserver;
 import deckfall.Observer.SwingGameView;
+import deckfall.Tower.Difficulty;
 import deckfall.Tower.Tower;
+import deckfall.Tower.TowerBuilder;
 
 public class Main {
     public static void main(String[] args) {
-        //ConsoleLogger view = new ConsoleLogger();
-        SwingGameView view = new SwingGameView();
-        Game game = new Game(new Slayer(), new Tower());
+        GameEventObserver view = selectView(args);
+        // Difficulty is set to Easy for now.
+        Tower tower = TowerBuilder.buildTower(Difficulty.EASY);
+        Game game = new Game(new Slayer(), tower);
         GameController controller = new GameController(game, view);
         controller.gameStart();
+    }
+
+    private static GameEventObserver selectView(String[] args) {
+        if (args.length > 0 && args[0].equalsIgnoreCase("console")) {
+            return new ConsoleLogger();
+        }
+        return new SwingGameView();
     }
 }
