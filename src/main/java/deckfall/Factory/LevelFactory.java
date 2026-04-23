cb95776cb80d4bf94432416abb79deace6f59c65
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-23 14:16:38 -0600

    Simplify LevelDifficulty to Difficulty, cleanup comments

--- src/main/java/deckfall/Factory/LevelFactory.java
+++ src/main/java/deckfall/Factory/LevelFactory.java
@@ -1,33 +1,37 @@
-package deckfall.Tower;
+package deckfall.Factory;
+import deckfall.Tower.*;
 import java.util.LinkedList;
 import java.util.concurrent.ThreadLocalRandom;
 
+import static deckfall.Factory.BattleFactory.createBattles;
+
+/**
+ * Early Game:  Levels 1 through 5 - facing 1-2 battle
+ * Late Game:   Levels 6 through 9 - facing 2-5 battles
+ * End Game:    Level 10 - Facing Final Boss Battle (Demon King)
+ */
 public class LevelFactory {
-    public static Level createLevel(LevelState levelState) {
+    public static Level createLevel(Difficulty levelState) {
         return switch (levelState) {
-            case EARLYGAME -> createEarlyGameLevel();
-            case LATEGAME -> createLateGameLevel();
-            case ENDGAME -> createEndGameLevel();
+            case EARLYGAME  -> createEarlyGameLevel();
+            case LATEGAME   -> createLateGameLevel();
+            case ENDGAME    -> createEndGameLevel();
+            default         -> createEarlyGameLevel();
         };
     }
-
-    /* Levels 1 through 5 - facing 1-2 battles */
     public static Level createEarlyGameLevel() {
         int battleCount = ThreadLocalRandom.current().nextInt(1, 3);
-        LinkedList<Battle> battles = BattleFactory.createBattles(battleCount, BattleDifficulty.EARLY);
+        LinkedList<Battle> battles = createBattles(battleCount, Difficulty.EARLYGAME);
         return new Level(battles);
     }
 
-    /* Levels 6 through 9 - facing 2-5 battles */
     public static Level createLateGameLevel() {
         int battleCount = ThreadLocalRandom.current().nextInt(2, 6);
-        LinkedList<Battle> battles = BattleFactory.createBattles(battleCount, BattleDifficulty.LATE);
+        LinkedList<Battle> battles = createBattles(battleCount, Difficulty.LATEGAME);
         return new Level(battles);
     }
 
-    /* Level 10 - Facing Final Boss Battle (Demon King) */
     public static Level createEndGameLevel() {
-        return new Level(deckfall.Tower.BattleFactory.createBattles(1, BattleDifficulty.BOSS));
+        return new Level(createBattles(1, Difficulty.ENDGAME));
     }
-
 }
