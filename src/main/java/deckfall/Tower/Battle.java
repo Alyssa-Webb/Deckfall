3d3ddf27d9a22f960cff2c45d4f31c8bbb1af82c
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-23 14:26:17 -0600

    Improved method naming, and added getters for entities in battle

--- src/main/java/deckfall/Tower/Battle.java
+++ src/main/java/deckfall/Tower/Battle.java
@@ -1,32 +1,68 @@
 package deckfall.Tower;
 
 import deckfall.Entity.Entity;
-
 import java.util.LinkedList;
 import java.util.List;
+import deckfall.Entity.Slayer;
 
 public class Battle {
     private final LinkedList<Entity> entities;
 
     public Battle(LinkedList<Entity> foes) {
-        this.entities = foes;
+        this.entities = new LinkedList<>(foes);
     }
 
+    // Battle is over when all non-player entities are dead
     public boolean battleOver() {
-        return entities.stream().noneMatch(Entity::isAlive);
+        return entities.stream()
+                .filter(e -> !(e instanceof Slayer))
+                .noneMatch(Entity::isAlive);
     }
 
-    public List<Entity> getFoes() {
-        return entities;
+    public boolean isPlayerDead() {
+        return entities.stream()
+                .filter(e -> e instanceof Slayer)
+                .noneMatch(Entity::isAlive);
     }
 
-    public void addPlayerCharacter(Entity player, int position){
+    // Returns only living non-player entities
+    public List<Entity> getActiveEnemies() {
+        return entities.stream()
+                .filter(e -> !(e instanceof Slayer) && e.isAlive())
+                .toList();
+    }
+
+    public List<Entity> getActivePlayers() {
+        return entities.stream()
+                .filter(e -> e instanceof Slayer && e.isAlive())
+                .toList();
+    }
+
+    // Returns all entities
+    public List<Entity> getEntities() { return entities.stream().toList(); }
+
+
+    // Player always goes first
+    public void addPlayerCharacter(Entity player) {
+        entities.addFirst(player);
+    }
+
+    public void addPlayerCharacter(Entity player, int position) {
         entities.add(position, player);
     }
 
-    public Entity getNextPlayer() {
-        Entity entity = entities.pop();
-        entities.addLast(entity);
-        return entity;
+    public Entity getNextTurn() {
+        if (entities.isEmpty()) {
+            return null;
+        }
+
+        for (int i = 0; i < entities.size(); i++) {
+            Entity entity = entities.pop();
+            entities.addLast(entity);
+            if (entity.isAlive()) {
+                return entity;
+            }
+        }
+        return null;
     }
 }
