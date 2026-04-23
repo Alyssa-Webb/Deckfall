0eb7e772b93ceb34ae6ac2a60f7613a91313b563
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-23 14:14:17 -0600

    Add a TargetType for GUI

--- src/main/java/deckfall/Card/AttackCard.java
+++ src/main/java/deckfall/Card/AttackCard.java
@@ -3,9 +3,8 @@ package deckfall.Card;
 public abstract class AttackCard extends Card {
     protected int damageDealt;
 
-    //Card(String cardName, int energyCost, String description, CardType type)
     public AttackCard(String name, int energyCost, String description, int damageDealt) {
-        super(name, energyCost, description, CardType.SLASH);
+        super(name, energyCost, description, CardType.SLASH, TargetType.ENEMY_ONLY);
         this.damageDealt = damageDealt;
     }
 }
