c7d379c053717944c1280629bc96bc83dd08376a
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-23 14:25:26 -0600

    Added getCurrentIntent() for GUI

--- src/main/java/deckfall/Entity/Enemy.java
+++ src/main/java/deckfall/Entity/Enemy.java
@@ -2,16 +2,18 @@ package deckfall.Entity;
 
 
 public abstract class Enemy extends Entity {
-    private static final int DEFAULT_BLOCK_AMOUNT = 0;
-
     public Enemy (String enemyName, int healthPoints) {
         super(enemyName, healthPoints);
     }
 
-    // Enemies decide their intent before their turn (during Player's turn)
+    public Enemy(String enemyName, int healthPoints, String description) {
+        super(enemyName, healthPoints, description);
+    }
+
     abstract public void decideIntent();
 
     abstract public void executeIntent(Slayer slayer);
 
+    abstract public IntentType getCurrentIntent();
 
 }
