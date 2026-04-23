cf6e5a6c8a1b2e3dbb1940ceba596be63aeab704
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-23 14:21:50 -0600

    Decoupled deck creation from SlayerFactory

--- src/main/java/deckfall/Factory/SlayerFactory.java
+++ src/main/java/deckfall/Factory/SlayerFactory.java
@@ -1,28 +1,15 @@
 package deckfall.Factory;
 
-import deckfall.Card.*;
 import deckfall.Entity.Slayer;
 
 
 public class SlayerFactory {
 
     public static Slayer createSlayer(String slayerName) {
-        Slayer slayer = new Slayer(slayerName);
-        buildStartingDeck(slayer);
-        return slayer;
+        return new Slayer(slayerName);
     }
 
     public static Slayer createSlayer(String slayerName, int healthPoints) {
-        Slayer slayer = new Slayer(slayerName, healthPoints);
-        buildStartingDeck(slayer);
-        return slayer;
-    }
-
-    private static void buildStartingDeck(Slayer slayer) {
-        slayer.addToDeck(new SlashCard());
-        slayer.addToDeck(new SlashCard());
-        slayer.addToDeck(new SlashCard());
-        slayer.addToDeck(new ShieldCard());
-        slayer.addToDeck(new ShieldCard());
+        return new Slayer(slayerName, healthPoints);
     }
 }
