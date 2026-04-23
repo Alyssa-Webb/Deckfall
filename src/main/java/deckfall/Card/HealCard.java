8b9ff117640add5cd8908a58f744814d87088430
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-23 14:16:18 -0600

    Add TargetType

--- src/main/java/deckfall/Card/HealCard.java
+++ src/main/java/deckfall/Card/HealCard.java
@@ -1,10 +1,14 @@
 package deckfall.Card;
 
 public abstract class HealCard extends Card {
-    private int healthHealed;
+    protected final int healthHealed;
 
     public HealCard(String name, int energyCost, String description, int healthHealed) {
-        super(name, energyCost, description, CardType.HEAL);
+        super(name, energyCost, description, CardType.HEAL, TargetType.SELF_ONLY);
         this.healthHealed = healthHealed;
+    }
+
+    public int getHealthHealed() {
+        return healthHealed;
     }
 }
