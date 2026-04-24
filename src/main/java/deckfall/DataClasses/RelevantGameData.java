480883dd0d6ac3e1595d8caf937a60016fe5ba79
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-24 13:49:41 -0600

    Add additional data to be stored

--- src/main/java/deckfall/DataClasses/RelevantGameData.java
+++ src/main/java/deckfall/DataClasses/RelevantGameData.java
@@ -6,26 +6,34 @@ import deckfall.Entity.Slayer;
 
 import java.util.List;
 
+// Java is suggesting this be a record class
 public class RelevantGameData {
     private final List<Card> cards;
     private final List<Entity> enemies;
     private final Slayer slayer;
+    private final int floorNumber;
+    private final int currentBattleIndex;
+    private final int totalBattlesOnCurrentFloor;
 
-    public RelevantGameData(List<Card> cards, List<Entity> enemies, Slayer slayer) {
+    public RelevantGameData(List<Card> cards, List<Entity> enemies, Slayer slayer, int floorNumber, int currentBattleIndex, int totalBattlesOnCurrentFloor ) {
         this.cards = cards;
         this.enemies = enemies;
         this.slayer = slayer;
+        this.floorNumber = floorNumber;
+        this.currentBattleIndex = currentBattleIndex;
+        this.totalBattlesOnCurrentFloor = totalBattlesOnCurrentFloor;
     }
 
-    public List<Card> getCards() {
-        return cards;
-    }
+    public List<Card> getCards() { return cards; }
 
-    public List<Entity> getEnemies() {
-        return enemies;
-    }
+    public List<Entity> getEnemies() { return enemies; }
 
-    public Slayer getSlayer() {
-        return slayer;
-    }
+    public Slayer getSlayer() { return slayer; }
+
+    public int getFloorNumber() { return floorNumber; }
+
+    public int getCurrentBattleIndex() { return currentBattleIndex; }
+
+    public int getTotalBattlesOnCurrentFloor() { return totalBattlesOnCurrentFloor; }
 }
+
