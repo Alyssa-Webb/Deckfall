efc5298172192ea61273ddf5e0862919d588e5e4
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-24 13:48:18 -0600

    Update game states

--- src/main/java/deckfall/Game/GameState.java
+++ src/main/java/deckfall/Game/GameState.java
@@ -1,9 +1,10 @@
 package deckfall.Game;
 
 public enum GameState {
-    GAME_START,                 // Game is running
-    PLAYER_TURN,                // It is the Player's (Slayer) turn
-    AWAITING_CONTINUE_PROMPT,   // Waiting for Player to continue to next level or battle
-    GAME_WIN,                   // Game is won
-    GAME_LOSS                   // Game is lost
+    GAME_START,                     // Game is running
+    PLAYER_TURN,                    // It is the Player's (Slayer) turn
+    NOTIFYING_OF_SIDE_EFFECTS,      // Pending SideEffects to drain (see Game.getSideEffect)
+    AWAITING_CONTINUE_PROMPT,       // Waiting for Player to continue to next level or battle
+    GAME_WIN,                       // Game is won
+    GAME_LOSS                       // Game is lost
 }
