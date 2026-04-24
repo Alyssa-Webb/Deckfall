9b4ca384ec5e754dde0d68bfb8fbd4e406cde3cf
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-24 12:16:57 -0600

    Simplify GameStates

--- src/main/java/deckfall/Game/GameState.java
+++ src/main/java/deckfall/Game/GameState.java
@@ -1,19 +1,9 @@
 package deckfall.Game;
 
 public enum GameState {
-    ENEMY_TURN,
-    PLAYER_TURN,
-
-    GAME_START,
-    GAME_OVER,
-    GAME_WIN,
-    GAME_LOSS,
-
-    LEVEL_START,
-    LEVEL_END,
-
-    BATTLE_START,
-    BATTLE_END,
-
-    NOTIFYING_OF_SIDE_EFFECTS,
+    GAME_START,                 // Game is running
+    PLAYER_TURN,                // It is the Player's (Slayer) turn
+    AWAITING_CONTINUE_PROMPT,   // Waiting for Player to continue to next level or battle
+    GAME_WIN,                   // Game is won
+    GAME_LOSS                   // Game is lost
 }
