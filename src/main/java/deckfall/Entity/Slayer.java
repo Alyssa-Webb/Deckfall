1525f425cbd8449ef09d142e26d8ac632082ea9c
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-25 12:56:58 -0600

    Add slayer description and getter

--- src/main/java/deckfall/Entity/Slayer.java
+++ src/main/java/deckfall/Entity/Slayer.java
@@ -7,6 +7,8 @@ public class Slayer extends Entity {
     private static final int DEFAULT_HEALTH = 50;
     private static final int DEFAULT_DRAW_COUNT = 5;
     private static final int DEFAULT_MAX_ENERGY = 3;
+    private static final String DEFAULT_SLAYER_DESCRIPTION = "You are the Slayer. You have been summoned by your realm to seek understanding behind the origin of missing persons.";
+
 
     private final int maxEnergy;
     private int energy;
@@ -22,8 +24,8 @@ public class Slayer extends Entity {
     public Slayer(String slayerName, int healthPoints) {
         super(slayerName, healthPoints);
         this.maxEnergy = DEFAULT_MAX_ENERGY;
-        this.energy    = maxEnergy;
-        this.block     = 0;
+        this.energy = maxEnergy;
+        this.block = 0;
         addToDeck(DEFAULT_CARD_DECK);
     }
 
@@ -37,6 +39,8 @@ public class Slayer extends Entity {
 
         return "";
     }
+
+    public String getDescription() { return DEFAULT_SLAYER_DESCRIPTION; }
 
     @Override
     public boolean isSlayer() {
