f14a972fda964fa86354715b33f160dca2bbb145
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-25 15:36:45 -0600

    Added small tower for testing

--- src/main/java/deckfall/Main.java
+++ src/main/java/deckfall/Main.java
@@ -4,16 +4,16 @@ import deckfall.Controller.GameController;
 import deckfall.Entity.Slayer;
 import deckfall.Game.Game;
 import deckfall.Observer.ConsoleLogger;
-import deckfall.Observer.SwingGameView;
+import deckfall.Factory.TowerFactory;
 import deckfall.Tower.Difficulty;
 import deckfall.Tower.Tower;
-import deckfall.Tower.TowerBuilder;
 
 public class Main {
     public static void main(String[] args) {
         ConsoleLogger view = new ConsoleLogger();
         //SwingGameView view = new SwingGameView();
-        Tower tower = TowerBuilder.buildTower(Difficulty.EASY);
+        // Tower tower = TowerFactory.createStandardTower(Difficulty.EASY);
+        Tower tower = TowerFactory.createSmallTower(Difficulty.EASY);
 
         Game game = new Game(new Slayer(), tower);
         GameController controller = new GameController(game, view);
