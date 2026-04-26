d74c3be6588f2cd17a1f9ab428c26eed4563d532
Alyssa Webb <amarie.19050874@gmail.com>
2026-04-25 20:29:21 -0600

    Add method coverage card tests

--- src/test/java/deckfall/Card/AttackCardTest.java
+++ src/test/java/deckfall/Card/AttackCardTest.java
@@ -1,4 +1,16 @@
 package deckfall.Card;
 
+import deckfall.Factory.CardFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AttackCardTest {
+
+    @Test
+    void testDefaultSlashCard () {
+        Card card = CardFactory.createCard(CardType.SLASH);
+        assertEquals( "Slash", card.getName());
+        assertEquals(1, card.getEnergyCost());
+    }
 }
--- src/test/java/deckfall/Card/ShieldCardTest.java
+++ src/test/java/deckfall/Card/ShieldCardTest.java
@@ -1,4 +1,15 @@
 package deckfall.Card;
 
+import deckfall.Factory.CardFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ShieldCardTest {
+    @Test
+    void testDefaultShieldCard () {
+        Card card = CardFactory.createCard(CardType.SHIELD);
+        assertEquals( "Shield Block", card.getName());
+        assertEquals(1, card.getEnergyCost());
+    }
 }
